b77e592982b48c7a04a60c0aff17efca0cd7f175
Moctar <mctrba@gmail.com>
2018-07-26 14:20:57 -0500

    reply/request/release protocol

--- main.c
+++ main.c
@@ -54,7 +54,7 @@ int handle_message(char* message, size_t length);
 
 void send_msg(int sockfd, char * buffer, int msglen);
 int receive_message(char * message, int length);
-int merge_timestamps(int * incoming_ts);
+int merge_timestamps(int incoming_ts);
 
 int message_source(char * msg);
 int message_dst(char * msg);
@@ -75,7 +75,7 @@ int port;
 
 Quorum_Member* quorum;
 int quorum_size;
-int lock_holder;
+int lock_holder = -1;
 int lock_received = 0;
 
 int main(int argc, char* argv[])
@@ -309,11 +309,15 @@ int handle_message(char* message, size_t length)
     strcpy(temp, message);
     temp[length] = '\0';
     printf("MSG RCVD: %s LENGTH: %d\n", temp, (int) length);
+    
+    int sender = message_source(message);
+    int sender_ts = message_ts(message);
+    merge_timestamps(sender_ts);
 
     if (message_type(message) == GRANT)
     {
         lock_received++;
-
+        // Check if all locks are received
         if (lock_received == quorum_size) {
             // Signal CS can be executed
             if (sem_post(&wait_grant) == -1) {
@@ -326,10 +330,25 @@ int handle_message(char* message, size_t length)
 
     if (message_type(message) == REQUEST)
     {
+        char msg[20];
+        if (lock_holder == -1) {
+            // Grant lock
+            timestamp++;
+            lock_holder = sender;
+            snprintf(msg, 9, "%02d%02dG%03d", node_id, sender, timestamp);
+            send_msg(quorum[sender].send_socket, msg, 8);
+        }
+
+        // If lock is already held check timestamps
+        else {
+
+        }
+        
     }
 
     if (message_type(message) == RELEASE)
     {
+        lock_holder = -1;
     }
 
     if (message_type(message) == FAILED)
@@ -442,6 +461,16 @@ void send_msg(int sockfd, char * buffer, int msglen)
     {
         bytes_to_send -= send(sockfd, buffer + (msglen - bytes_to_send), msglen, 0);
     }
+}
+
+int merge_timestamps(int incoming_ts)
+{
+    if (incoming_ts > timestamp)
+        timestamp = incoming_ts + 1;
+    else
+        timestamp = timestamp + 1;
+
+    return 0;
 }
 
 // Message accessor functions, for easy reading
